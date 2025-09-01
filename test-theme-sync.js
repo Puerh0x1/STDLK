868933add39eab209aeabbd06ce9b960ba92d40f
Puerh0x1 <puerh0x1@protonmail.ch>
2025-09-01 11:00:37 +0700

    f

--- test-theme-sync.js
+++
@@ -1,4 +0,0 @@
-// Проверка синхронизации темы
-console.log("Текущая тема из localStorage:", localStorage.getItem("modern-theme"));
-console.log("Текущая тема на странице:", document.documentElement.getAttribute("data-theme"));
-console.log("BroadcastChannel поддерживается:", typeof BroadcastChannel !== "undefined");
